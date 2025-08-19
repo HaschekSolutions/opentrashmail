2d965bc30b8dd86488036e06fd2bf2868377da40
Christian Haschek <christian@haschek.at>
2025-08-19 07:16:57 +0000

    disabling auto refresh until a smarter implementation is found. fixes Ajax hx-trigger "load, every 30s" breaks firefox Fixes #106

--- web/templates/index.html.php
+++ web/templates/index.html.php
@@ -25,7 +25,7 @@
   <button class="htmx-indicator" aria-busy="true">Loading…</button>
 
   <main id="main" class="container" hx-get="/api/<?= $url ?>" hx-trigger="load, every 30s">
-
+    <main id="main" class="container" hx-get="/api/<?= $url ?>" hx-trigger="load">
   </main>
 
   <script src="/js/opentrashmail.js"></script>
